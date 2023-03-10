ea7801cab749624f2f520d2dede9bc019f549782
Ryan Kline <kliner@mail.gvsu.edu>
2023-03-10 17:31:04 -0500

    Set up control and data connections. Sends EOF error after 2nd command

--- client/ftp_client.go
+++ client/ftp_client.go
@@ -14,18 +14,16 @@ import (
 	"net"
 	"os"
 	"regexp"
+	"strings"
 )
 
 const (
-	SERVER_HOST = "localhost"
-	SERVER_PORT = "8636"
 	SERVER_TYPE = "tcp"
 )
 
 func main() {
 	var (
 		command string
-		msg     string
 		pattern = `^CONNECT ([a-zA-Z0-9\-\.]+:[0-9]+)$`
 	)
 
@@ -39,37 +37,73 @@ func main() {
 		}
 
 		if matched, err := regexp.MatchString(pattern, command); err == nil && matched {
-			connection, err := net.Dial(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+			splitCommand := strings.Split(command, " ")
+			hostAndPort := strings.Split(splitCommand[1], ":")
+			host := hostAndPort[0]
+			port := hostAndPort[1]
+
+			//Control Connection
+			control, err := establishConnection("Control", host, port)
 			if err != nil {
-				panic(err)
-			} else {
-				fmt.Println(fmt.Sprintf("Connected to %s:%s", SERVER_HOST, SERVER_PORT))
+				continue
 			}
 
 			///send some data
 			for {
-				fmt.Println("Write a message to send to the server:")
-				fmt.Scanln(&msg)
+				fmt.Println("Enter a command:")
+				if scanner.Scan() {
+					command = scanner.Text()
+				}
 
-				if msg != "QUIT" {
-					_, _ = connection.Write([]byte(msg))
+				//TODO: Do some preprocessing to separate the command type from potential command args
+				//TODO: Make create an array of all valid command types
+
+				//Data Connection - established and torn down after each command
+				data, err := establishConnection("Data", host, port)
+				if err != nil {
+					continue
+				}
+
+				if command != "QUIT" {
+					_, _ = control.Write([]byte(command))
 					buffer := make([]byte, 1024)
-					mLen, err := connection.Read(buffer)
+					mLen, err := control.Read(buffer)
 					if err != nil {
 						fmt.Println("Error reading:", err.Error())
 					}
 					fmt.Println("Received: ", string(buffer[:mLen]))
+					defer func(data net.Conn) {
+						err := data.Close()
+						if err != nil {
+							fmt.Println(err)
+						}
+					}(data)
 				} else {
-					fmt.Println(fmt.Sprintf("Ending connection with %s:%s", SERVER_HOST, SERVER_PORT))
+					fmt.Println(fmt.Sprintf("Ending connection with %s:%s", host, port))
+					defer data.Close()
 					break
 				}
+
 			}
-			defer connection.Close()
+			defer control.Close()
+
 		} else if command == "QUIT" {
 			break
 		} else {
-			fmt.Println("Command does not match pattern")
+			fmt.Println("Invalid Command. Use `CONNECT <server name/IP address> <server port>` to connect to " +
+				"a server")
 		}
 	}
 
+}
+
+func establishConnection(connectionType, host, port string) (net.Conn, error) {
+	connection, err := net.Dial(SERVER_TYPE, host+":"+port)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(fmt.Sprintf("[%s] Connected to %s:%s", connectionType, host, port))
+	}
+
+	return connection, err
 }
